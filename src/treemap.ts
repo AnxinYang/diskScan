27022ecfe47f441d67e54c3b5a6294168c93baba
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2019-09-06 22:37:26 -0700

    add scss/ convert to div

--- src/treemap.ts
+++ src/treemap.ts
@@ -1,18 +1,20 @@
 import { ccd3 as d3 } from 'ccts';
 
+const SIZE: [number, number] = [500, 500];
+
+
 export default function (containerID: string, data: {}) {
     console.log(containerID);
     let svg = d3.select(`#${containerID}`)
-        .append('svg')
-        .attr('width', 500)
-        .attr('height', 500);
+        .append('div')
+        .style('position', `relative`)
+        .style('width', `100vw`)
+        .style('height', `100vh`);
 
     let treemap = d3.treemap()
-        .size([500, 500])
+        .size(SIZE)
         .tile(d3.treemapResquarify)
-        .round(true)
-        .paddingInner(5)
-        .paddingOuter(16);
+        .round(true);
 
     let root = d3.hierarchy(data);
 
@@ -22,22 +24,42 @@ export default function (containerID: string, data: {}) {
 
     treemap(root);
 
-    let nodes = svg.append('g')
-        .selectAll('g')
+    let nodes = svg.append('section')
+        .style('width', `100%`)
+        .style('height', `100%`)
+        .selectAll('div')
         .data(root.descendants())
         .enter()
-        .append('g')
-        .attr('transform', function (d: any) { return 'translate(' + [d.x0, d.y0] + ')' });
-
-    nodes.append('rect')
-        .attr('fill', 'rgba(255, 165, 2, .3)')
-        .attr('width', function (d: any) { return d.x1 - d.x0; })
-        .attr('height', function (d: any) { return d.y1 - d.y0; });
-
-    nodes.append('text')
-        .attr('dx', 4)
-        .attr('dy', 14)
+        .append('div')
+        .classed('warper', true)
+        .style('transform', function (d: any) { return `translate(${NTP(d.x0)}vw, ${NTP(d.y0)}vh)` })
+        .style('width', function (d: any) { return NTP(d.x1 - d.x0) + '%'; })
+        .style('height', function (d: any) { return NTP(d.y1 - d.y0) + '%'; })
+        .append('div')
+        .attr('id', function (d: { data: any }) {
+            return d.data.path;
+        })
+        .style('background-color', 'rgba(255, 165, 2, .3)')
+        .style('width', '100%')
+        .style('height', '100%')
+        .style('overflow', 'hidden')
+        .append('header')
+        .classed('title', true)
+        .style('padding', '8px')
         .text(function (d: { data: any }) {
             return d.data.name;
-        })
+        });
+
+    // nodes.append('text')
+    //     .attr('dx', 4)
+    //     .attr('dy', 14)
+    //     .text(function (d: { data: any }) {
+    //         return d.data.name;
+    //     })
+}
+
+
+function NTP(px: number) {
+    //return px;
+    return px * 100 / SIZE[0];
 }
