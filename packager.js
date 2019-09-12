8f4ae623a6edcee50bab7c168a118d502be6aa4b
Anxin <anxinyang114@gmail.com>
2019-09-12 15:43:11 -0700

    change name

--- packager.js
+++ packager.js
@@ -1,7 +1,13 @@
 const packager = require('electron-packager');
+const path = require('path');
 
 packager({
-    dir: './',
-    icon: './build/icon.ico',
+    dir: path.join(__dirname, ''),
+    icon: path.join(__dirname, '/build/icon.ico'),
     overwrite: true,
+    arch: 'x64',
+    platform: 'win32',
 })
+    .then(function () {
+        console.log('DONE...')
+    })
