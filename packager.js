33877af75b5bbef0ed8c0eb656513167ddad8ab4
AnxinYang <32281036+AnxinYang@users.noreply.github.com>
2019-09-12 21:18:45 -0700

    get packager working

--- packager.js
+++ packager.js
@@ -11,30 +11,33 @@ const isProduction = argv.includes('production');
 console.log(`Compiling ${isProduction ? 'production' : 'development'} code:`)
 
 const config = isProduction ? config_production : config_dev;
-console.log(config)
-const compiler = webpack(config, function () {
-    if (isProduction) {
-        packager({
-            dir: path.join(__dirname, ''),
-            icon: path.join(__dirname, '/build/icon.ico'),
-            overwrite: true,
-            arch: 'x64',
-            platform: 'win32',
-            prune: true,
-            out: path.join(__dirname, '/ssa'),
-            asar: true,
-        })
-            .then(function () {
-                console.log('Done...')
+if (!isProduction) {
+    webpack(config)
+        .watch(config.watchOptions, function (err, stats) {
+            console.log(stats.toString({
+                chunks: false,
+                colors: true
+            }));
+        });
+} else {
+    webpack(config, function () {
+        if (isProduction) {
+            packager({
+                dir: path.join(__dirname, ''),
+                icon: path.join(__dirname, '/build/icon.ico'),
+                overwrite: true,
+                arch: 'x64',
+                platform: 'win32',
+                prune: true,
+                out: path.join(__dirname, '/ssa'),
+                asar: true,
             })
-    }
-})
+                .then(function () {
+                    console.log('Done...')
+                })
+        }
+    })
+}
 
-const watching = compiler.watch(undefined, function (err, stats) {
-    console.log(stats || '');
-    console.error(err || '');
-});
 
-(function wait() {
-    if (true) setTimeout(wait, 1000);
-})();
+
